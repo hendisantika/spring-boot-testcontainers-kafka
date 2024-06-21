2d8f1b640eb9f2c3c179ea665fd333d755d98d7d
hendisantika <hendisantika@yahoo.co.id>
2024-06-21 07:49:24 +0700

    Add sendMessage in KafkaController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/estcontainerskafka/controller/KafkaController.java
+++ src/main/java/id/my/hendisantika/estcontainerskafka/controller/KafkaController.java
@@ -2,9 +2,17 @@ package id.my.hendisantika.estcontainerskafka.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +31,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class KafkaController {
     private static final String TOPIC = "kafka_topic";
     private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @GetMapping(path = "/{message}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> sendMessage(@PathVariable("message") String message) {
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(TOPIC, "key",
+                message);
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                log.info("Sent message=[" + message + "] with offset=["
+                        + result.getRecordMetadata().offset() + "]");
+            } else {
+                log.warn("Unable to send message=[" + message + "] due to : " + ex.getMessage());
+            }
+        });
+        return new ResponseEntity<>(
+                "Finish sending [" + message + "], Check log to know the result", HttpStatus.OK);
+    }
 }
