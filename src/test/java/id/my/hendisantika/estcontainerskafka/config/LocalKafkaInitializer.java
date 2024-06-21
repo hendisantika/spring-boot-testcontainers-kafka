1067d12808f970e9ffe65bf35d243172fe75e370
hendisantika <hendisantika@yahoo.co.id>
2024-06-21 07:54:28 +0700

    Add setProperties in LocalKafkaInitializer🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/estcontainerskafka/config/LocalKafkaInitializer.java
+++ src/test/java/id/my/hendisantika/estcontainerskafka/config/LocalKafkaInitializer.java
@@ -4,11 +4,15 @@ import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.shaded.org.checkerframework.checker.nullness.qual.NonNull;
 import org.testcontainers.utility.DockerImageName;
 
-import static java.lang.System.setProperties;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -37,5 +41,15 @@ public class LocalKafkaInitializer implements
                 .withEnv("KAFKA_CREATE_TOPICS", "kafka_topic");
         kafka.start();
         setProperties(environment, "kafka.config.bootstrap-server", kafka.getBootstrapServers());
+    }
+
+    private void setProperties(ConfigurableEnvironment environment, String name, Object value) {
+        MutablePropertySources sources = environment.getPropertySources();
+        PropertySource<?> source = sources.get(name);
+        if (source == null) {
+            source = new MapPropertySource(name, new HashMap<>());
+            sources.addFirst(source);
+        }
+        ((Map<String, Object>) source.getSource()).put(name, value);
     }
 }
