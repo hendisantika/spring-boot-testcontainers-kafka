67f9b335f6993bb342b6ee1f60b8171cbdd47c1c
hendisantika <hendisantika@yahoo.co.id>
2024-06-21 07:52:43 +0700

    Add LocalKafkaInitializer🫸🌀✏️📗🐧🐳⬆

---
+++ src/test/java/id/my/hendisantika/estcontainerskafka/config/LocalKafkaInitializer.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.estcontainerskafka.config;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testcontainers-kafka
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/06/24
+ * Time: 07.52
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class LocalKafkaInitializer implements
+        ApplicationContextInitializer<ConfigurableApplicationContext> {
+}
