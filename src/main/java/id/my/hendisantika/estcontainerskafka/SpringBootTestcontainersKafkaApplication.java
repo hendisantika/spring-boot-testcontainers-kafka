527a7aadfaf915cdee500468cbf2d27ce8be0c65
hendisantika <hendisantika@yahoo.co.id>
2024-06-21 07:46:13 +0700

    Add KafkaConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/estcontainerskafka/SpringBootTestcontainersKafkaApplication.java
+++ src/main/java/id/my/hendisantika/estcontainerskafka/SpringBootTestcontainersKafkaApplication.java
@@ -1,9 +1,12 @@
 package id.my.hendisantika.estcontainerskafka;
 
+import id.my.hendisantika.estcontainerskafka.config.KafkaProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(KafkaProperties.class)
 public class SpringBootTestcontainersKafkaApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/id/my/hendisantika/estcontainerskafka/config/KafkaConfig.java
@@ -0,0 +1,35 @@
+package id.my.hendisantika.estcontainerskafka.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testcontainers-kafka
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/06/24
+ * Time: 07.44
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public ProducerFactory<String, String> producerFactory(KafkaProperties kafkaProperties) {
+        return new DefaultKafkaProducerFactory<>(kafkaProperties.kafkaConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(KafkaProperties kafkaProperties,
+                                                       ProducerFactory<String, String> producerFactory) {
+        System.setProperty("java.security.krb5.conf", kafkaProperties.getKrb5Conf());
+        System.setProperty("java,security,auth.login.config", kafkaProperties.getLoginConf());
+        System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
+        System.setProperty("sun.security.krb5.debug", kafkaProperties.getKrb5Debug());
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
