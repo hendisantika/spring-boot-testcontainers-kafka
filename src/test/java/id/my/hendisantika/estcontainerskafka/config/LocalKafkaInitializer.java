8c32156ea7133cae6b96e80f87a00256e9775564
hendisantika <hendisantika@yahoo.co.id>
2024-06-21 07:53:29 +0700

    Add kafkaLocalSetup in LocalKafkaInitializer🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/estcontainerskafka/config/LocalKafkaInitializer.java
+++ src/test/java/id/my/hendisantika/estcontainerskafka/config/LocalKafkaInitializer.java
@@ -3,7 +3,12 @@ package id.my.hendisantika.estcontainerskafka.config;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.shaded.org.checkerframework.checker.nullness.qual.NonNull;
+import org.testcontainers.utility.DockerImageName;
+
+import static java.lang.System.setProperties;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,5 +27,15 @@ public class LocalKafkaInitializer implements
     @Override
     public void initialize(@NonNull ConfigurableApplicationContext context) {
         kafkaLocalSetup(context);
+    }
+
+    private void kafkaLocalSetup(ConfigurableApplicationContext context) {
+        ConfigurableEnvironment environment = context.getEnvironment();
+        KafkaContainer kafka = new KafkaContainer(
+                DockerImageName.parse("confluentinc/cp-kafka:7.2.2.arm64"))
+                .withEnv("KAFKA_AUTO_CREATE_TOPICS_ENABLE", "true")
+                .withEnv("KAFKA_CREATE_TOPICS", "kafka_topic");
+        kafka.start();
+        setProperties(environment, "kafka.config.bootstrap-server", kafka.getBootstrapServers());
     }
 }
